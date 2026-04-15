9dc013e3bd257fe9746912a45d474b45e81e8bac
luumat <mpholucia840@gmail.com>
2026-04-15 13:52:12 +0200

    Update Sales.java

--- src/com/tuckshop/Sales.java
+++ src/com/tuckshop/Sales.java
@@ -1,3 +1,13 @@
+package com.tuckshop;
+    
 public class Sales {
-    // your code
+    private Product product;
+    private int quantitySold;
+    private String paymentMethod;
+
+public Sale(Product product, int quantitySold, String paymentMethod) {
+    this.product = product;
+    this.quantitySold = quantitySold;
+    this.paymentMethod = paymentMethod;
+  }
 }
