ea520bbc35583529a10f29218d1c96c84efc1c80
Reuben Tlotlo Sianga <reubensianga2@gmail.com>
2026-04-15 15:41:41 +0200

    Update Sales.java

--- src/com/tuckshop/Sales.java
+++ src/com/tuckshop/Sales.java
@@ -1,13 +1,3 @@
-package com.tuckshop;
-    
 public class Sales {
-    private Product product;
-    private int quantitySold;
-    private String paymentMethod;
-
-public Sale(Product product, int quantitySold, String paymentMethod) {
-    this.product = product;
-    this.quantitySold = quantitySold;
-    this.paymentMethod = paymentMethod;
-  }
+ 
 }
