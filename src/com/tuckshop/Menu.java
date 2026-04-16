e003cf6606ff9d9ac15960126e9150f1fcf97a1f
ru447 <reubensianga2@gamil.com>
2026-04-16 07:24:33 +0200

    feat: added the menu loop, please change or add something to the code

--- src/com/tuckshop/Menu.java
+++ src/com/tuckshop/Menu.java
@@ -1,6 +1,40 @@
 //package
+package com.tuckshop;
 
+import java.util.Scanner;
 //this class is for the menu loop 
 public class Menu {
    //type you code for menu here 
+
+   
+
+    public void displayMenu(){
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter 1. Show Tuckshop Products   2.Exit ");
+        int number= in.nextInt();
+        //pre conditions 
+        int count= 0;
+        do{
+            
+            switch(number){
+                case 1: 
+                    System.out.println("Sweets");
+                    System.out.println("Soft drinks");
+                    System.out.println("biscuits");
+                    System.out.println("chips");
+                    break;
+                case 2:
+                    System.out.println("thank you and come again next time");
+                    break;
+               default:
+                  System.out.println("Please enter a valid option");
+                    
+            }
+            
+            count++;
+        } while(count!=1);
+        
+    }
+
 }
+
